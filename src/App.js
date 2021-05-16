68a946e2b4f05eba15afa29a213026e4267540ff
taiwocoker <taiwofcoker@gmail.com>
2021-05-16 16:23:47 +0100

    Check if styles are okay

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
